eee87ddec68326b1b29b543159cbf8ea2d20af18
Igor Aguiar <desenvigor@gmail.com>
2022-10-09 10:56:50 -0300

    First Commit

---
+++ src/main/java/com/example/DeliveryCenter/DeliveryCenterApplication.java
@@ -0,0 +1,13 @@
+package com.example.DeliveryCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryCenterApplication.class, args);
+	}
+
+}
