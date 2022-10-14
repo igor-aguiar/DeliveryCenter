c49ad7e685a9b7dd5967d7b7604c9c21ce149190
igor-aguiar <desenvigor@gmail.com>
2022-10-14 20:21:21 -0300

    Create navbar

--- src/main/java/com/example/DeliveryCenter/controller/OrderController.java
+++ src/main/java/com/example/DeliveryCenter/controller/OrderController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Controller
 @RequestMapping("order")
 public class OrderController {
@@ -12,6 +15,8 @@ public class OrderController {
     @GetMapping("register")
     public ModelAndView register(){
         ModelAndView mv = new ModelAndView("order/register");
+        LocalDateTime orderTime = LocalDateTime.now();
+        mv.addObject("orderTime", orderTime.format(DateTimeFormatter.ofPattern("dd/MM/yy HH:mm")));
         return mv;
     }
 }
